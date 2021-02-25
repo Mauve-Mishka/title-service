584a734733923732a1d909219ec6ce633ccadd2d
Manish Makin <sunnymakin@gmail.com>
2021-02-24 20:37:41 -0600

    express server completed with title API

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const { TitleSchema } = require('./TitleSchema.js');
+const { TitleSchema } = require('./Schema.js');
 const { config } = require('../config.js');
 
 const { db: {
@@ -8,5 +8,24 @@ const { db: {
   name
 } } = config;
 
-mongoose.connect(`mongodb://${host}:${port}/${name}`);
+mongoose.connect(`mongodb://${host}:${port}/${name}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
+const Title = mongoose.model('Title', TitleSchema);
+
+const getTitleByRoomId = (roomId) => {
+  return new Promise((resolve, reject) => {
+    Title.find({roomId})
+    .then((result) => {
+      resolve(result);
+    })
+    .catch((err) => {
+      reject(err);
+    });
+  })
+
+}
+
+module.exports.getTitleByRoomId = getTitleByRoomId;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const { getTitleByRoomId } = require('../database/index.js');
 const PORT = 5006;
 
-app.get('/', (req, res) => {
-  res.send('title service is up!');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/rooms/:id/title', async (req, res) => {
+  try {
+    let title = await getTitleByRoomId(req.params.id);
+    res.status(200).send(title);
+  } catch(e) {
+    res.status(500).send(e)
+  }
 });
 
 app.listen(PORT, () => {
